e941e62aa87b7082152e128fd65291c64d0b3a88
ZufarSuleimanov <z.suleimanov@icloud.com>
2021-02-19 15:33:40 +0600

    first commit

---
+++ Test_Task_LeadIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Task_LeadIT
+//
+//  Created by Zufar Suleimanov on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
