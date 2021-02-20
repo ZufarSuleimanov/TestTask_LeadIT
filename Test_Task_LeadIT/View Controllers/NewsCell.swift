97dbad678c34821509a598b8a780aa9cfcc7a34e
ZufarSuleimanov <z.suleimanov@icloud.com>
2021-02-20 18:12:56 +0600

    Added: main cell view for main view "news feed"

---
+++ Test_Task_LeadIT/View Controllers/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  Test_Task_LeadIT
+//
+//  Created by Zufar Suleimanov on 19.02.2021.
+//
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
