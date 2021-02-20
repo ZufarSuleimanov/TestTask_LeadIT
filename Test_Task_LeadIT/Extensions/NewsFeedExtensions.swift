1875efaf4677ee82c3f458ced20f46daae933dc3
ZufarSuleimanov <z.suleimanov@icloud.com>
2021-02-20 18:12:06 +0600

    Added: main view and extension

---
+++ Test_Task_LeadIT/Extensions/NewsFeedExtensions.swift
@@ -0,0 +1,49 @@
+//
+//  NewsFeedExtensions.swift
+//  Test_Task_LeadIT
+//
+//  Created by Zufar Suleimanov on 19.02.2021.
+//
+
+import UIKit
+
+let itemsPerRow: CGFloat = 1
+let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+
+extension NewsFeedViewController: UICollectionViewDelegate { }
+
+extension NewsFeedViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cells.NewsFeedCell.rawValue, for: indexPath) as! NewsCell
+        return cell
+    }
+
+}
+
+extension NewsFeedViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingWidth = sectionInserts.left * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInserts
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInserts.left
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInserts.left
+    }
+}
---
+++ Test_Task_LeadIT/View Controllers/NewsFeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewsFeedViewController.swift
+//  Test_Task_LeadIT
+//
+//  Created by Zufar Suleimanov on 19.02.2021.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    @IBOutlet weak var newsFeedCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newsFeedCollectionView.delegate = self
+        newsFeedCollectionView.dataSource = self
+        newsFeedCollectionView.register(UINib.init(nibName: XIBs.NewsCell.rawValue, bundle: nil), forCellWithReuseIdentifier: Cells.NewsFeedCell.rawValue)
+    }
+
+}
